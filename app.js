424b830a4505f1b1fc13f2a6c5134a8142940982
learndev77 <restrella@77global.biz>
2024-07-03 12:15:47 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const feedRoutes = require('./routes/feed')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+// Access-Control-Allow-Origin: http://foo.example
+// Access-Control-Allow-Methods: POST, GET
+// Access-Control-Allow-Headers: X-PINGOTHER, Content-Type
+
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    next();
+})
+
+app.use('/feed', feedRoutes)
+
+app.listen(8080)
